0e94145b9d9f16adc9fdd1472aad099b4a04f8c1
rjgates3 <gates.j.richard@gmail.com>
2019-09-05 09:05:36 -0600

    added auth-router

--- thingful-client/src/components/App/App.js
+++ thingful-client/src/components/App/App.js
@@ -32,18 +32,15 @@ class App extends Component {
               path={'/'}
               component={ThingListPage}
             />
-            <Route
-            // <PublicOnlyRoute
+            <PublicOnlyRoute
               path={'/login'}
               component={LoginPage}
             />
-            <Route
-            // <PublicOnlyRoute
+            <PublicOnlyRoute
               path={'/register'}
               component={RegistrationPage}
             />
-            <Route
-            // <PrivateRoute
+            <PrivateRoute
               path={'/thing/:thingId'}
               component={ThingPage}
             />
