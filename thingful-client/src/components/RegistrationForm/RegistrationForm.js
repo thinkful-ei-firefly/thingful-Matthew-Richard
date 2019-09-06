ecd47365ec88ef71250c01bf3d7cd9e16791609b
rjgates3 <gates.j.richard@gmail.com>
2019-09-06 11:48:01 -0600

    added user login tests

--- thingful-client/src/components/RegistrationForm/RegistrationForm.js
+++ thingful-client/src/components/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react'
-import { Button, Input, Required } from '../Utils/Utils'
+'use strict';
+
+import React, { Component } from 'react';
+import { Button, Input, Required } from '../Utils/Utils';
+import AuthApiService from '../../serviceWorker/AuthApiService';
 
 export default class RegistrationForm extends Component {
   static defaultProps = {
@@ -12,15 +15,28 @@ export default class RegistrationForm extends Component {
     ev.preventDefault()
     const { full_name, nick_name, user_name, password } = ev.target
 
-    console.log('registration form submitted')
-    console.log({ full_name, nick_name, user_name, password })
+    this.setState({
+      error: null
+    });
 
-    full_name.value = ''
-    nick_name.value = ''
-    user_name.value = ''
-    password.value = ''
-    this.props.onRegistrationSuccess()
-  }
+    AuthApiService.postUser({
+      user_name: user_name.value,
+      password: password.value,
+      full_name: full_name.value,
+      nickname: nick_name.value
+    })
+      .then(user => {
+        full_name.value = ''
+        nick_name.value = ''
+        user_name.value = ''
+        password.value = ''
+        this.props.onRegistrationSuccess()
+      })
+      .catch(res => {
+        this.setState({
+          error: res.error
+        })
+      })
 
   render() {
     const { error } = this.state
